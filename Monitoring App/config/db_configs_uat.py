c0ac9a50e7eb68ee3aee3d5c20a45399ee3fb7f5
Saad Ahmed <saad.ahmed@cubastion.com>
2026-09-03 15:34:42 +0530

    latest version 03092026

--- Monitoring App/config/db_configs_uat.py
+++
@@ -1,5 +0,0 @@
-# ---------------- config/db_configs_uat.py ----------------
-ENVIRONMENTS = ["UAT1"]
-DB_CONFIGS = {
-    "UAT1": {"user": "ODIADMIN", "password": "od9adm9n2022", "dsn": "SIEBQA"}
-}
--- Monitoring App/core/template_engine.py
+++
@@ -1,7 +0,0 @@
-# ---------------- core/template_engine.py ----------------
-class TemplateEngine:
-    @staticmethod
-    def render(sql_template, row_data):
-        bind_vars = {k.upper():v for k,v in row_data.items()}
-        return sql_template, bind_vars
-
